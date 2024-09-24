80e0833a914bde40c4debbf884a78d09631e1629
lucazcortez <lucazcortez2@outlook.com>
2024-09-24 16:43:40 -0300

    Conta banco1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String [] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Digite o número da sua Conta:");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua Agência:");
+            agencia = scanner.next();
+
+            System.out.println("Digite o seu nome:");
+            nomeCliente = scanner.next();
+
+            System.out.println("Digite o saldo da sua conta:");
+            saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + " obrigado por criar conta em nosso banco,sua agência é" + agencia + ", conta " + numeroConta + "e o seu saldo" + saldo + " já está disponive para saque.");
+        }
+
+        }
+    
